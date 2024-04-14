2e5763c72d4f72241d96d2ac6f7dc91828ab43bf
Julien Guillen <723506@student.dusd.net>
2024-04-14 01:25:51 -0700

    got the UI
    need to make a hunger system 4/14/24

---
+++ Unit2BasicGameplayJLNP8/Assets/Scripts/AnimalHunger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalHunger : MonoBehaviour
+{
+    public Slider hungerSlider;
+    public int hunger;
+
+    private int currentHunger = 0;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hungerSlider.maxValue = hunger;
+        hungerSlider.value = 0;
+        hungerSlider.fillRect.gameObject.SetActive(false);
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    public void FeedAnimal(int amount)
+    {
+        currentHunger += amount;
+        hungerSlider.fillRect.gameObject.SetActive(true);
+        hungerSlider.value = currentHunger;
+
+        if(currentHunger >= hunger)
+        {
+            gameManager.AddScore(hunger);
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
--- Unit2BasicGameplayJLNP8/Assets/Scripts/GameManager.cs
+++ Unit2BasicGameplayJLNP8/Assets/Scripts/GameManager.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int score = 0;
-    public int lives = 3;
+    int score = 0;
+    int lives = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AddLives(int value)
+    {
+        lives += value;
+        if (score <= 0)
+        {
+            Debug.Log("Game Over");
+            lives = 0;
+        }
+        Debug.Log("Lives = " + lives);
+    }
+    public void AddScore(int value)
+    {
+        score += value;
+        Debug.Log("Score = " +  score);
     }
 }
