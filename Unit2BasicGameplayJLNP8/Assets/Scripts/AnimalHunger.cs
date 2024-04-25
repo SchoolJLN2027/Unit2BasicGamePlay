10a1280be01ebbfb92fbdbb9258879f3a6a688d3
Julien Guillen <723506@student.dusd.net>
2024-04-25 00:59:01 -0700

    unit 2 done
    4/25/24

--- Unit2BasicGameplayJLNP8/Assets/Scripts/AnimalHunger.cs
+++ Unit2BasicGameplayJLNP8/Assets/Scripts/AnimalHunger.cs
@@ -5,25 +5,25 @@ using UnityEngine.UI;
 
 public class AnimalHunger : MonoBehaviour
 {
-    public Slider hungerSlider;
-    public int hunger;
+    //public Slider hungerSlider;
+    public int hunger = 1;
 
     private int currentHunger = 0;
     private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        hungerSlider.maxValue = hunger;
-        hungerSlider.value = 0;
-        hungerSlider.fillRect.gameObject.SetActive(false);
+       // hungerSlider.maxValue = hunger;
+       // hungerSlider.value = 0;
+       // hungerSlider.fillRect.gameObject.SetActive(false);
 
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
     public void FeedAnimal(int amount)
     {
         currentHunger += amount;
-        hungerSlider.fillRect.gameObject.SetActive(true);
-        hungerSlider.value = currentHunger;
+       // hungerSlider.fillRect.gameObject.SetActive(true);
+       // hungerSlider.value = currentHunger;
 
         if(currentHunger >= hunger)
         {
